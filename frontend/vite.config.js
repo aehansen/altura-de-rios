81895b78e740badee67955c4dd580fa3bf1a5956
Andres Enrique Hansen <aehansen@PREFECTURA.NET>
2026-04-21 09:26:45 -0300

    Arquitectura Inicial PWA (Removido Workflow)

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  base: '/rio-paranacito/', // Cambiar al nombre de tu repo en GitHub
+  base: '/altura-de-rios/', // Adaptado a tu repo de GitHub
   plugins: [
     react(),
     VitePWA({
